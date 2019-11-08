60f6ccda98e991f5fbabbeb18be22a19aa47f7c7
Tim Fränkert <tim@fraenkert.de>
2019-11-08 10:54:09 +0100

    upt1
    upt1

--- Bomberguy/Spieler.java
+++ Bomberguy/Spieler.java
@@ -7,34 +7,40 @@ import GLOOP.*;
  */
 public class Spieler
 {
-    private GLKegel spieler;
+    private GLObjekt[] spieler ;
     public Spieler(int x, int y)
     {
-        spieler = new GLKegel(x,y,0, 20, 20);
-        spieler.setzeTextur("grun.jpg");
+        spieler = new GLObjekt[8];
+        spieler[0] = new GLKugel(x,y,-8, 8,"weis.jpg" );
+        spieler[1] = new GLKugel(x,y,-21, 6,"weis.jpg" );
+        spieler[2] = new GLKugel(x,y,-29, 3,"weis.jpg" );
+        spieler[3] = new GLKugel(x-2,y+2,-29, 1,"schwarz.jpg" );
+        spieler[4] = new GLKugel(x+2,y+2,-29, 1,"schwarz.jpg" );
+        spieler[5] = new GLZylinder(x,y,-35,3,5,"grun.jpg");
+        spieler[6] = new GLZylinder(x,y,-32,5,1,"grun.jpg");
     }
     
     public void movUp(){
-        spieler.verschiebe(5,0,0);
+        spieler[0].verschiebe(5,0,0);
     }
     
     public void movDown(){
-        spieler.verschiebe(-5,0,0);
+        spieler[0].verschiebe(-5,0,0);
     }
     
     public void movLeft(){
-        spieler.verschiebe(0,-5,0);
+        spieler[0].verschiebe(0,-5,0);
     }
     
     public void movRight(){
-        spieler.verschiebe(0,5,0);
+        spieler[0].verschiebe(0,5,0);
     }
     
     public double getX(){
-        return spieler.gibX();
+        return spieler[0].gibX();
     }
     
     public double getY(){
-        return spieler.gibY();
+        return spieler[0].gibY();
     }
 }
